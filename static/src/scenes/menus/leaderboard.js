584fed7d4f6b6264bbe47a220a5a4cb7419c71e2
Chaktim <chaktimw@buffalo.edu>
2021-10-16 18:49:55 -0400

    leaderboard.js updated

--- static/src/scenes/menus/leaderboard.js
+++ static/src/scenes/menus/leaderboard.js
@@ -6,36 +6,38 @@ export class leaderboard extends Phaser.Scene {
         super(Constants.Scenes.leaderboard);
     }
     //Put in database information
-    init(){}
-    leaderboardData;
+    init(){
+
+    }
+    leaderboardData = null;
     preload(){
+        var parent = this;
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function(){
+        xhr.onreadystatechange = function () {
             if (this.readyState != 4) return;
             if (this.status == 200) {
-                // this.leaderboardData contains json received from the server
-                this.leaderboardData = JSON.parse(this.responseText);
+                // data var contains json received from the server
+                parent.leaderboardData = JSON.parse(this.responseText);
             }
-        }
+        };
         xhr.open('GET', '/get-leaderboard', true);
         xhr.send();
+        // console.log(this.leaderboardData);
+        // setTimeout(() => {console.log(this.leaderboardData)}, 1000);
     }
     //make the leaderboard using the data grabbed in init
     create(){
-        this.leaderboardText = new Phaser.GameObjects.Text(this, 100, 90,'LEADERBOARD', {fill: '#d4b2d8', align: 'center'});
-        this.leaderboardText.setFontSize(40);
-        this.add.existing(this.leaderboardText);
-        this.nameText = new Phaser.GameObjects.Text(this, 100, 120, 'NAME - SCORE - LEVEL', {fill: '#d4b2d8', align: 'center'});
-        this.nameText.setFontSize(32);
-        this.add.existing(this.nameText);
+        setTimeout(() => {
+            this.leaderboardText = new Phaser.GameObjects.Text(this, 275, 200, 'LEADERBOARD', {fill: '#ffffff'});
+            this.leaderboardText.setFontSize(72);
+            this.add.existing(this.leaderboardText);
+            this.nameText = new Phaser.GameObjects.Text(this, 250, 230, 'NAME - SCORE - LEVEL', {fill: '#ffffff'});
+            this.nameText.setFontSize(48);
+            this.add.existing(this.nameText);
+            console.log(this.leaderboardData)
 
-        // Iterating through the leaderboard Data
-        for (let score of this.leaderboardData){
-            console.log(score)
-        }
-
-        this.menuButton = new TextButton(this, 25, 550, 'BACK', {fill: '#ffffff'}, {fill: '#888888'}, 48, ()=>this.scene.start(Constants.Scenes.mainMenu));
-        this.add.existing(this.menuButton);
-        // console.log('leader testing');
+            this.menuButton = new TextButton(this, 25, 550, 'BACK', {fill: '#ffffff'}, {fill: '#888888'}, 48, () => this.scene.start(Constants.Scenes.mainMenu));
+            this.add.existing(this.menuButton);
+            }, 100);
     }
 }
