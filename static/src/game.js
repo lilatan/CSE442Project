18006e907de9a10f09497429d23be28381d3e59e
eyang6 <36167153+eyang6@users.noreply.github.com>
2021-10-16 02:55:55 -0400

    Level 1 scene created and connected to main menu

--- static/src/game.js
+++ static/src/game.js
@@ -2,13 +2,21 @@ import { leaderboard } from "./scenes/menus/leaderboard.js";
 import { mainMenu } from "./scenes/menus/mainmenu.js";
 import { optionsMenu } from "./scenes/menus/options.js";
 import { loadGame } from "./scenes/menus/loadgamemenu.js";
+import { level1 } from "./scenes/game/level1.js";
 //figure out way to not import every single scene in here
 
 var config = {
-    width:1000,
-    height:800,
+    width:800,
+    height:600,
     backgroundColor: 0x000000,
     // transparent: true,
-    scene:[mainMenu, leaderboard, optionsMenu, loadGame]
+    scene:[mainMenu, leaderboard, optionsMenu, loadGame, level1],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 700},
+            debug: true
+        }
+    }
 }
 var crewel = new Phaser.Game(config);
--- static/src/scenes/game/level1.js
+++ static/src/scenes/game/level1.js
@@ -3,9 +3,117 @@ export class level1 extends Phaser.Scene {
     constructor(){
         super(Constants.Scenes.lvl1);
     }
+    player;
+    coin;
+    platforms;
+    cursors;
+    crewels = 0;
+    coinCount;
+
+    keyW;
+    keyA;
+    keyS;
+    keyD;
+
+    keyESC;
+
+
+    init(){
+
+    }
+
+    preload(){
+        this.load.image('background', '/static/src/assets/sand_gw2.png');
+        this.load.image('ground', '/static/src/assets/sand_platform.png');
+        this.load.image('this.coin', '/static/src/assets/single_coin.png');
+        this.load.image('player_one', '/static/src/assets/spear_player.png');
+    }
 
     create(){
+
+        console.log("im at level 1");
+        this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+        this.keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
         
+        
+
+        this.add.image(400, 300, 'background');
+
+        this.platforms = this.physics.add.staticGroup();
+
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        this.platforms.create(150, 300, 'ground').setScale(0.5).refreshBody();
+        this.platforms.create(400, 230, 'ground').setScale(0.5).refreshBody();
+        this.platforms.create(100, 400, 'ground');
+        this.platforms.create(700, 450, 'ground');
+        this.platforms.create(550, 150, 'ground');
+        this.platforms.create(25, 125, 'ground');
+
+        this.player = this.physics.add.sprite(100, 450, 'player_one');
+
+        this.player.setBounce(0.1);
+        this.player.setCollideWorldBounds(true);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.coin = this.physics.add.group({
+            key: 'coin',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
+
+        this.coin.children.iterate(function (child) {
+
+            child.setBounceY(Phaser.Math.FloatBetween(0.2, 0.4));
+
+        });
+
+        this.coinCount = this.add.text(16, 16, 'crewels: 0', { fontSize: '32px', fill: '#000' });
+
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.coin, this.platforms);
+
+        this.physics.add.overlap(this.player, this.coin, this.collectcoin, null, this);
+
     }
+
+    update(){
+        if (this.cursors.left.isDown || this.keyA.isDown)
+        {
+            this.player.setVelocityX(-200);
+
+         
+        }
+        else if (this.cursors.right.isDown || this.keyD.isDown)
+        {
+            this.player.setVelocityX(200);
+        }
+        else
+        {
+            this.player.setVelocityX(0); 
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down || this.keyW.isDown && this.player.body.touching.down)
+        {
+            this.player.setVelocityY(-400);
+        }
+        if (this.cursors.down.isDown || this.keyS.isDown)
+        {
+            this.player.setVelocityY(-170);
+        }
+    }
+
+    collectcoin (player, coin){
+        coin.disableBody(true, true);
+
+        this.crewels += 1;
+        this.coinCount.setText('crewels: ' + this.crewels);
+    }
+
 
 }
