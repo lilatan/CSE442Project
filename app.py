0b885b13adfc06a94d9f58ea6c3e68fc511b2067
Chaktim <chaktimw@buffalo.edu>
2021-10-16 15:43:15 -0400

    added get-leaderboard path to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, send_from_directory
 import pymongo
 import os
+import json
 from database import database_tools, tests
 
 app = Flask(__name__, template_folder='templates')
@@ -16,6 +17,12 @@ def staticFolder(name):
     return send_from_directory(
         'static', name
     )
+
+
+@app.route("/get-leaderboard")
+def getLeaderboard():
+    return json.dumps(database_tools.get_leaderboard())
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
