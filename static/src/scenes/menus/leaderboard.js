1e259a5634746d0a32b70900e36fa3b37ac3a59a
eyang6 <36167153+eyang6@users.noreply.github.com>
2021-10-16 15:41:28 -0400

    leaderboard text added

--- static/src/scenes/menus/leaderboard.js
+++ static/src/scenes/menus/leaderboard.js
@@ -14,6 +14,14 @@ export class leaderboard extends Phaser.Scene {
     }
     //make the leaderboard using the data grabbed in init
     create(){
+        this.leaderboardText = new Phaser.GameObjects.Text(this,275,200,'LEADERBOARD', {fill: '#ffffff'});
+        this.leaderboardText.setFontSize(72);
+        this.add.existing(this.leaderboardText);
+        this.nameText = new Phaser.GameObjects.Text(this, 250, 230, 'NAME - SCORE - LEVEL', {fill: '#ffffff'});
+        this.nameText.setFontSize(48);
+        this.add.existing(this.nameText);
+
+        
         this.menuButton = new TextButton(this, 25, 550, 'BACK', {fill: '#ffffff'}, {fill: '#888888'}, 48, ()=>this.scene.start(Constants.Scenes.mainMenu));
         this.add.existing(this.menuButton);
         // console.log('leader testing');
