0264ce4ccd309a539b8fbd5b62635d39ae3960a2
eyang6 <36167153+eyang6@users.noreply.github.com>
2021-10-16 15:27:41 -0400

    app.py changes+phaser typecript for intellisense

--- app.py
+++ app.py
@@ -17,38 +17,6 @@ def staticFolder(name):
         'static', name
     )
 
-
-# @app.route("/static/src/<path:name>")
-# def source(name):
-#     return send_from_directory(
-#         'src', name
-#     )
-
-
-
-# @app.route('/leaderboard')
-# def leaderboard():
-#     collection = db['testData']
-#     # Code to add data to a specific collection on mongodb
-#     # data = [
-#     #     {"name": "Cheese"}
-#     # ]
-#     # collection.insert(data)
-#     print("Testing print functionality in the heroku server logs", flush=True)
-#     print(collection.find_one(), flush=True)
-#     return render_template('leaderboard.html')
-
-
-# @app.route('/load')
-# def load():
-#     return render_template('load.html')
-
-
-# @app.route('/static/<path:filename>')
-# def jscript(filename):
-#     return render_template('static', filename)
-
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     app.run(host="0.0.0.0", port=port)
