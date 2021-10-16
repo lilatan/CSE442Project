e0d920be6ef5814ea64cd253e3916b738c4d1ba1
eyang6 <36167153+eyang6@users.noreply.github.com>
2021-10-16 18:21:28 -0400

    BGM added

--- static/src/game.js
+++ static/src/game.js
@@ -3,6 +3,7 @@ import { mainMenu } from "./scenes/menus/mainmenu.js";
 import { optionsMenu } from "./scenes/menus/options.js";
 import { loadGame } from "./scenes/menus/loadgamemenu.js";
 import { pauseMenu } from "./scenes/menus/pausemenu.js";
+import { mainMenuLoad } from "./scenes/menus/mainLoadingScreen.js";
 import { level1 } from "./scenes/game/level1.js";
 //figure out way to not import every single scene in here
 
@@ -11,13 +12,16 @@ var config = {
     height:600,
     backgroundColor: 0x000000,
     // transparent: true,
-    scene:[mainMenu, leaderboard, optionsMenu, loadGame,pauseMenu, level1],
+    scene:[mainMenuLoad, mainMenu, leaderboard, optionsMenu, loadGame,pauseMenu, level1],
     physics: {
         default: 'arcade',
         arcade: {
             gravity: {y: 700},
             debug: true
         }
-    }
+    },
+    audio:{
+        disableWebAudio: true
+    },
 }
 var crewel = new Phaser.Game(config);
---
+++ static/src/scenes/menus/mainLoadingScreen.js
@@ -0,0 +1,27 @@
+import { Constants } from "/static/src/Constants.js";
+
+export class mainMenuLoad extends Phaser.Scene {
+    constructor(){
+        super(Constants.Scenes.mainMenuLoad);
+    }
+
+    keySpace;
+    preload(){
+        var loading = new Phaser.GameObjects.Text(this, 200,200,'LOADING...', {fill: '#ffffff'});
+        this.load.audio('bgm', '/static/src/assets/bgm/Prologue.mp3');
+    }
+    create(){
+        var music = this.sound.add('bgm');
+        
+        var prompt = new Phaser.GameObjects.Text(this, 200,200,'Press SPACE', {fill: '#ffffff'});
+        prompt.setFontSize(72);
+        this.add.existing(prompt);
+        this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.keySpace.on('up',()=>this.next());
+        // this.scene.start(Constants.Scenes.mainMenu);
+        music.play();
+    }
+    next(){
+        this.scene.start(Constants.Scenes.mainMenu);
+    }
+}
