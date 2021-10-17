9e1d715cae7ef1aa6db0e8cf755a52f5be689e97
Chaktim <chaktimw@buffalo.edu>
2021-10-16 22:39:10 -0400

    #16 Added leaderboard data to display

--- static/src/scenes/menus/leaderboard.js
+++ static/src/scenes/menus/leaderboard.js
@@ -20,8 +20,27 @@ export class leaderboard extends Phaser.Scene {
                 parent.leaderboardData = JSON.parse(this.responseText);
 
                 // Iterating through the leaderboard Data
-                for (let score of parent.leaderboardData){
-                    console.log(score)
+                let x = 100 ; let y = 160; // position of first entry
+                let max_entries = 10; let entry_num = 0; // limit number of entries on leaderboard
+                for (let entry of parent.leaderboardData){
+                    // stop adding entries if limit hit
+                    if (entry_num === max_entries) break;
+                    else entry_num += 1;
+
+                    // Get each field from an entry
+                    let entry_name = "" + entry.name;
+                    let entry_score = "" + entry.score;
+                    let entry_level = "" + entry.level;
+
+                    // Pad name and score to align columns
+                    while (entry_name.length < 12) {entry_name += " "}
+                    while (entry_score.length < 12) {entry_score += " "}
+
+                    let value = entry_name + entry_score + entry_level;
+                    parent.leaderboardText = new Phaser.GameObjects.Text(parent, x, y, value, {fill: '#d4b2d8', align: 'center'});
+                    parent.leaderboardText.setFontSize(20);
+                    parent.add.existing(parent.leaderboardText);
+                    y += 30
                 }
                 // PUT UPDATE LEADERBOARD PAGE CODE HERE
             }
@@ -32,7 +51,7 @@ export class leaderboard extends Phaser.Scene {
 
     //make the leaderboard page (static content only)
     create(){
-        this.leaderboardText = new Phaser.GameObjects.Text(this, 100, 90, 'LEADERBOARD', {fill: '#d4b2d8', align: 'center'});
+        this.leaderboardText = new Phaser.GameObjects.Text(this, 100, 80, 'LEADERBOARD', {fill: '#d4b2d8', align: 'center'});
         this.leaderboardText.setFontSize(40);
         this.add.existing(this.leaderboardText);
         this.nameText = new Phaser.GameObjects.Text(this, 100, 120, 'NAME - SCORE - LEVEL', {fill: '#d4b2d8', align: 'center'});
