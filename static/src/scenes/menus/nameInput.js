6ff179b9ef1933de98cf33447ac54c18b25a8eaa
Chaktim <chaktimw@buffalo.edu>
2021-11-07 17:05:15 -0500

    #77 Updated nameInput.js to be the gameover scene

--- static/src/scenes/menus/nameInput.js
+++ static/src/scenes/menus/nameInput.js
@@ -1,4 +1,5 @@
-import { Constants } from "../../Constants.js";
+import { Constants } from "/static/src/Constants.js";
+import { TextButton } from "/static/src/game_objects/TextButton.js";
 
 export class nameInput extends Phaser.Scene{
     constructor(){
@@ -6,48 +7,55 @@ export class nameInput extends Phaser.Scene{
     }
     score;
     level;
-    username;
-    element;
+    userInput;
     init(data){//[Score, Game Scene]
         this.score = data[0];
         this.level = data[1];
     }
     preload(){
-        this.load.html('nameform', '/static/src/assets/html/nameform.html');
+        this.screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
 
-        var name = prompt("Please enter your name", "user");
-        if (name != null) {
-            this.send_leaderboard_entry(name, this.score, this.level.key);
-        }
-        this.scene.stop(this.level.key);
-        this.scene.start(Constants.Scenes.mainMenu);
+        this.load.image('graveyard', '/static/src/assets/images/graveyard2.jpg');
+        // <a href="https://www.freepik.com/vectors/tree">Tree vector created by upklyak - www.freepik.com</a>
+
+        // this.load.html('nameform', '/static/src/assets/html/nameform.html');
     }
     create(){
-        // this.scene.bringToTop();
-        // var prompt = this.add.text(300,100, "Enter your name", {fill: '#ffffff', fontSize: 48});
-        // this.element = this.add.dom(200, 200).createFromCache('nameform');
-        // this.element.addListener('click');
-        // this.element.on('click', ()=>this.getUsername());
+        // add image to background and scale it
+        let image = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'graveyard')
+        let scaleX = this.cameras.main.width / image.width
+        let scaleY = this.cameras.main.height / image.height
+        let scale = Math.max(scaleX, scaleY)
+        image.setScale(scale).setScrollFactor(0)
 
-        // this.tweens.add({
-        //     targets: this.element,
-        //     y: 300,
-        //     duration: 3000,
-        //     ease: 'Power3'
-        // });
+        // "game over" text at the top of the scene
+        this.gameoverText = this.add.text(this.screenCenterX, 70,
+            'GAME OVER',
+            { fontSize: '80px', fill: '#730000' }
+        ).setOrigin(0.5);
+
+        // prompt user to enter name
+        this.promptText = this.add.text(this.screenCenterX, 150,
+            'ENTER YOUR NAME',
+            { fontSize: '32px', fill: '#ffffff' }
+        ).setOrigin(0.5);
+
+        // player input for name
+        this.nameDisplay = this.add.text(this.screenCenterX, 200,
+            'NAME',
+            { fontSize: '40px', fill: '#000000' }
+        ).setOrigin(0.5);
+
+        // update display with user's input
+        // this.nameDisplay.setText("this is my name");
+
+        // back to menu button
+        this.menuButton = new TextButton(this, 25, 550, 'BACK TO MAIN MENU', {fill: '#ffffff'}, {fill: '#999999'}, 32,
+            ()=> {this.scene.start(this.data.key); this.sound.play(Constants.SFX.back)});
+        this.add.existing(this.menuButton);
+
     }
 
-    // getUsername(event){
-    //     if(event.target.name ==='enterButton'){
-    //         this.username = this.element.getChildByName('nameField');
-    //         if(this.username !==''){
-    //             this.element.removeListener('click');
-    //             this.send_leaderboard_entry(this.username, this.score, this.level.key);
-    //             this.scene.stop(this.level.key);
-    //             this.scene.start(Constants.Scenes.leaderboard);
-    //         }
-    //     }
-    // }
     send_leaderboard_entry(name, score, level){
         var xhr = new XMLHttpRequest();
         xhr.open("POST", '/update-leaderboard', true);
